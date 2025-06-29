f49dd78049e6bc24b012afc6014ab3b2ae8b74ff
Kamal Koranga <kamalkoranga75@gmail.com>
2025-06-29 12:53:57 +0530

    added react router for multi page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import SignUp from './pages/SignUp.jsx'
+import LogIn from './pages/LogIn.jsx'
+import NotFoundPage from './pages/NotFound.jsx'
+import Dashboard from './pages/Dashboard.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Dashboard />
+  },
+  {
+    path: '/signup',
+    element: <SignUp />
+  },
+  {
+    path: '/login',
+    element: <LogIn />
+  },
+  {
+    path: '*',
+    element: <NotFoundPage />
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,9 +1,9 @@
-const App = () => {
+const SignUp = () => {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold">Hello, World!</h1>
+      <h1 className="text-4xl font-bold">Sign Up</h1>
     </div>
   );
 }
 
-export default App;
+export default SignUp;
