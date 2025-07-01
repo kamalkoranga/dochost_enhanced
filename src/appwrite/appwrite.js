7a8ef079f03b73e90196d5004535e5687533faa2
Kamal Koranga <kamalkoranga75@gmail.com>
2025-07-01 07:58:57 +0530

    create user document at user register and get user document from db

--- src/appwrite/appwrite.js
+++ src/appwrite/appwrite.js
@@ -1,20 +1,56 @@
-import { Account, Client, ID } from "appwrite";
+import { Account, Client, Databases, ID, Permission, Role } from "appwrite";
 
 class AuthService {
   client = new Client();
+  databaseId = import.meta.env.VITE_APPWRITE_DATABASE_ID;
+  collectionId = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
   account;
+  database;
 
   constructor() {
     this.client
       .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT)
       .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID);
     this.account = new Account(this.client);
+    this.database = new Databases(this.client);
   }
+
+  async createUserDocument(userId) {
+    try {
+      const document = await this.database.createDocument(this.databaseId, this.collectionId, ID.unique(), {
+        userId,
+      }, [
+        // only any and guest permission
+        Permission.read(Role.any(userId)),
+        Permission.write(Role.any(userId))
+      ]);
+      return document;
+    } catch (error) {
+      console.log("Appwrite service :: createUserDocument :: error:", error);
+      return null;
+    }
+  }
+
+  async getUserDocument(userId) {
+    try {
+      const allDocuments = await this.database.listDocuments(this.databaseId, this.collectionId);
+      const userDocument = allDocuments.documents.find(doc => doc.userId === userId);
+      return userDocument || null;
+    } catch (error) {
+      console.log("Appwrite service :: getUserDocument :: error:", error);
+      return null;
+    }
+  }
+
 
   async createAccount({email, password, name}) {
     try {
       const userAccount = await this.account.create(ID.unique(), email, password, name);
       if (userAccount) {
+        // create a document in the database to store total size of user storage
+        const result = await this.createUserDocument(userAccount.$id);
+        // console.log(result)
+
         // call another method like directly login the user
         return this.login({email, password});
       } else {
