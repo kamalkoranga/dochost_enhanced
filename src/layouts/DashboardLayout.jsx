4a130a0ae23d970074b195f8de93bcae5ef2d631
Kamal Koranga <kamalkoranga75@gmail.com>
2025-07-02 21:14:34 +0530

    fetch user then check its subscription staus every time component loads  and then according to it make changes

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -1,8 +1,10 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import UploadModal from "../components/UploadModal";
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
 import { Outlet } from "react-router-dom";
+import subscriptionService from "../appwrite/subscriptions";
+import authService from "../appwrite/appwrite";
 
 
 const DashboardLayout = () => {
@@ -10,6 +12,39 @@ const DashboardLayout = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [refreshFiles, setRefreshFiles] = useState(0);
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [activePlan, setActivePlan] = useState('Cloud Plan');
+  const [currentUser, setCurrentUser] = useState(null);
+
+  // check if user's subscription is active
+  // if not, call the revertToFreePlan method from subscriptionService
+  useEffect(() => {
+    const fetchCurrentUser = async () => {
+      try {
+        const user = await authService.getCurrentUser();
+        if (!user) {
+          console.error("No user is currently logged in.");
+          return;
+        }
+        // console.log("Current User:", user);
+        setCurrentUser(user);
+        checkSubscription(user.$id);
+      } catch (error) {
+        console.error("Error fetching current user:", error);
+      }
+    };
+    fetchCurrentUser();
+
+    const checkSubscription = async (userId) => {
+      const timeLimit = await subscriptionService.getTimeLimit(userId);
+      const document = await subscriptionService.getDocumentByUserID(userId);
+      if (timeLimit && document.plan !== "free" && timeLimit < new Date()) {
+        console.log("Subscription expired, reverting to free plan.");
+        await subscriptionService.revertToFreePlan(userId);
+        setRefreshFiles(prev => prev + 1); // Refresh files after reverting
+        setActivePlan("Cloud Plan"); // Update active plan to free
+      }
+    };
+  }, []);
 
   return (
     <div className="min-h-screen bg-gray-50">
@@ -30,7 +65,7 @@ const DashboardLayout = () => {
 
         {/* Outlet is used to render nested routes, if any */}
         <main className="flex-1 lg:ml-0">
-          <Outlet context={{ refreshFiles, setRefreshFiles, viewMode }}/>
+          <Outlet context={{ refreshFiles, setRefreshFiles, viewMode, activePlan, setActivePlan }}/>
         </main>
 
       </div>
